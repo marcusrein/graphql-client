0187dda1a3c7d193754e25f31dfc5d74d5f97a53
Marcus Rein <64141593+marcusrein@users.noreply.github.com>
2023-02-05 09:09:43 -0500

    got the votes to display

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,84 @@
 <template>
-  <div></div>
+	<div>
+		<ul
+			v-for="(strategy, strategyIndex) in result.votingStrategies"
+			:key="strategyIndex"
+		>
+			<ul v-for="(vote, voteIndex) in strategy.votes" :key="voteIndex">
+				<li>{{ vote.from }}</li>
+				<li>{{ vote.to }}</li>
+				<li>{{ vote.token }}</li>
+
+				<li>{{ vote.amount }}</li>
+				<li>{{ vote.createdAt }}</li>
+				<li>{{ vote.id }}</li>
+				<li>{{ vote.projectId }}</li>
+				<li>{{ vote.version }}</li>
+			</ul>
+		</ul>
+	</div>
 </template>
 
 <script>
+import { useQuery } from "@vue/apollo-composable";
+import gql from "graphql-tag";
+
+const ALL_VOTES_QUERY = gql`
+	query VoteQuery {
+		votingStrategies {
+			strategyName
+			strategyAddress
+			votes(orderBy: amount) {
+				amount
+				createdAt
+				from
+				id
+				projectId
+				to
+				token
+				version
+			}
+		}
+	}
+`;
+
+// const ALL_BOOKS_QUERY = gql`
+// 	query AllBooks {
+// 		allBooks {
+// 			id
+// 			title
+// 			rating
+// 		}
+// 	}
+// `;
+export default {
+	name: "App",
+	setup() {
+		const { result } = useQuery(ALL_VOTES_QUERY);
+
+		console.log(result);
+		return { result };
+	},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 
 .list-wrapper {
-  display: flex;
-  margin: 0 auto;
-  max-width: 960px;
+	display: flex;
+	margin: 0 auto;
+	max-width: 960px;
 }
 
 .list {
-  width: 50%;
+	width: 50%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,80 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp, h, provide } from "vue";
+import App from "./App.vue";
+import {
+	ApolloClient,
+	createHttpLink,
+	InMemoryCache,
+} from "@apollo/client/core";
+import gql from "graphql-tag";
+import { DefaultApolloClient } from "@vue/apollo-composable";
 
-createApp(App).mount('#app')
+// const httpLink = createHttpLink({
+// 	uri: "http://localhost:4000/graphql",
+// });
+
+const httpLink = createHttpLink({
+	uri: "https://api.thegraph.com/subgraphs/name/gitcoinco/grants-round-goerli-testnet",
+});
+
+const cache = new InMemoryCache();
+
+const apolloClient = new ApolloClient({
+	link: httpLink,
+	cache,
+});
+
+const ALL_VOTES_QUERY = gql`
+	query InitQuery {
+		votingStrategies {
+			strategyName
+			strategyAddress
+			votes(orderBy: from) {
+				amount
+				createdAt
+				from
+				id
+				projectId
+				to
+				token
+				version
+			}
+		}
+	}
+`;
+
+const ALL_BOOKS_QUERY = gql`
+	query AllBooks {
+		allBooks {
+			id
+			title
+			rating
+		}
+	}
+`;
+
+// const ALL_BOOKS_QUERY = gql`
+// 	query AllBooks {
+// 		allBooks {
+// 			id
+// 			title
+// 			rating
+// 		}
+// 	}
+// `;
+
+// apolloClient
+// 	.query({
+// 		query: ALL_VOTES_QUERY,
+// 	})
+// 	.then((res) => {
+// 		console.log(res);
+// 	});
+
+const app = createApp({
+	setup() {
+		provide(DefaultApolloClient, apolloClient);
+	},
+	render: () => h(App),
+});
+
+app.mount("#app");
